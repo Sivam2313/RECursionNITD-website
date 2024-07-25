35c22d216b995362efb87f8a03708a3d4b8ba3ca
sivam <sivamdas7@gmail.com>
2024-07-25 21:42:04 +0530

    deployment

--- website/website/settings.py
+++ website/website/settings.py
@@ -96,14 +96,18 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-         'NAME': 'mydb',
-         'USER': 'myprojectuser',
-         'PASSWORD': 'password',
-         'HOST': 'localhost',
-         'PORT': '',
-     }
+    #  'default': {
+    #      'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #      'NAME': 'mydb',
+    #      'USER': 'myprojectuser',
+    #      'PASSWORD': 'password',
+    #      'HOST': 'localhost',
+    #      'PORT': '',
+    #  }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR + '/db.sqlite3',
+    }
 }
 
 
