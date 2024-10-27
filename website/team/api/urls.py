4225ac0d8c2c0f43c2d0299d9945c65143ee7e3a
Shibshankar0909 <123229119+Shibshankar0909@users.noreply.github.com>
2024-10-27 18:04:11 +0530

    created API to get alumni year wise

--- website/team/api/urls.py
+++ website/team/api/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from .views import (
     ListCreateMembersView,
+    AlumniYearWiseView
 )
 
 app_name = 'team_api'
 
 urlpatterns = [
     path('', ListCreateMembersView.as_view(), name='members_list'),
+    path('alumni/', AlumniYearWiseView.as_view(), name='alumni_year_wise'),
 ]
